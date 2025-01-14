247b98c4be355b06517bd59cbf8dafb0aa571f1b
jesusm2000 <ivanmoreiraloor2019@hotmail.com>
2025-01-13 21:45:47 -0500

    Prueba

---
+++ src/cooptransporte/CoopTransporte.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package cooptransporte;
+
+/**
+ *
+ * @author Jesus
+ */
+public class CoopTransporte {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // Definimos dos números
+        int numero1 = 5;
+        int numero2 = 10;
+
+        // Realizamos la suma
+        int resultado = numero1 + numero2;
+
+        // Mostramos el resultado en la consola
+        System.out.println("La suma de " + numero1 + " y " + numero2 + " es: " + resultado);
+    }
+    
+}
